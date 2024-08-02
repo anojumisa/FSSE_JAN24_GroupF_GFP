e7a012645ebe27257ded7befd3099e89b5b79fa8
Ano Jumisa <anojumisa@gmx.de>
2024-08-02 18:28:44 +0800

    feat: add testimonials section for landing page

--- frontend/final_project/src/components/layouts/Landing_Page/Testimonials/index.tsx
+++ frontend/final_project/src/components/layouts/Landing_Page/Testimonials/index.tsx
@@ -1,7 +1,25 @@
 export default function Testimonials() {
     return (
-        <div>
-            <h1>Testimonials</h1>
-        </div>
+        <div className="w-full h-screen flex flex-col items-center justify-center gap-8">
+			<h1 className="text-4xl text-gray-500 leading-relaxed text-center w-1/2">What our customers say about us</h1>
+            <h1 className="text-4xl text-gray-600 leading-relaxed text-center w-4/5">
+				"Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum has been the industry's standard dummy text ever
+				since the 1500s"
+			</h1>
+			<div className="flex items-center gap-4">
+				<div className="rounded-full w-12 h-12 bg-black overflow-hidden">
+					<img src="https://rairaksa.github.io/assets/img/rai.jpg" />
+				</div>
+				<div className="flex flex-col tracking-wider">
+					<label className="text-gray-600 font-bold text-base">
+						Rai Raksa Muhamad
+					</label>
+					<label className="text-gray-400 font-normal text-sm">
+						Fullstack PHP Developer
+					</label>
+				</div>
+			</div>
+		</div>
     )
 }

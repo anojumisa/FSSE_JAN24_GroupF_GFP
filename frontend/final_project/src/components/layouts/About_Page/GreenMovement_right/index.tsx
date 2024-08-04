f5ae851fc3d79fb41b039b7e3cbc67e1d0f6187f
Ano Jumisa <anojumisa@gmx.de>
2024-08-04 22:51:04 +0800

    feat: add about page

---
+++ frontend/final_project/src/components/layouts/About_Page/GreenMovement_right/index.tsx
@@ -0,0 +1,78 @@
+interface GreenMovement_rightProps {
+	title: string;
+	description: string;
+	movement: string;
+	image: string;
+	checklist_one: string;
+	checklist_two: string;
+	checklist_three: string;
+	checklist_four: string;
+}
+
+export default function GreenMovement_right({
+	title,
+	description,
+	movement,
+	image,
+	checklist_one,
+	checklist_two,
+	checklist_three,
+	checklist_four,
+}: GreenMovement_rightProps) {
+	return (
+		<div className="py-4 relative overflow-hidden bg-white ">
+			<div className="grid grid-cols-2 max-w-screen-lg mx-auto">
+				<div className="py-20 bg-amber-500 relative before:absolute before:h-full before:w-screen before:bg-amber-500 before:top-0 before:right-0">
+					<div className="relative z-20 pl-12 ">
+						<h2 className="text-blue-950 font-black text-5xl leading-snug mb-10">
+							{title}
+						</h2>
+						<p className="text-sky-950 text-lg">{description}</p>
+
+						<div className="flex items-start gap-4">
+							<span className="w-5 rounded-lg bg-green-800 mt-2">
+								<img src="checkmark.svg" alt="" />
+							</span>
+							<div>
+								<h2 className="text-sky-950 text-lg">{checklist_one}</h2>
+							</div>
+						</div>
+						<div className="flex items-start gap-4">
+							<span className="w-5 rounded-lg bg-green-800 mt-2">
+								<img src="checkmark.svg" alt="" />
+							</span>
+							<div>
+								<h2 className="text-sky-950 text-lg">{checklist_two}</h2>
+							</div>
+						</div>
+						<div className="flex items-start gap-4">
+							<span className="w-5 rounded-lg bg-green-800 mt-2">
+								<img src="checkmark.svg" alt="" />
+							</span>
+							<div>
+								<h2 className="text-sky-950 text-lg">{checklist_three}</h2>
+							</div>
+						</div>
+						<div className="flex items-start gap-4">
+							<span className="w-5 rounded-lg bg-green-800 mt-2">
+								<img src="checkmark.svg" alt="" />
+							</span>
+							<div>
+								<h2 className="text-sky-950 text-lg">{checklist_four}</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="w-full flex flex-col pl-16">
+					<h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">
+						{movement}
+					</h2>
+
+					<div className="h-full mt-auto overflow-hidden relative">
+						<img src={image} className="h-full w-full object-contain" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}

48f521e47a0117276c6a150cd602ed80fd93071c
Fachminur1 <139152663+Fachminur1@users.noreply.github.com>
2024-08-07 21:50:25 +0700

    Delete frontend/final_project/src/components/layouts/Dashboard_User directory

--- frontend/final_project/src/components/layouts/Dashboard_User/Notifications/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Notification = () => (
-  <div className="bg-gray-100 p-4 rounded">
-    Here Comes The Notification
-  </div>
-);
-
-export default Notification;

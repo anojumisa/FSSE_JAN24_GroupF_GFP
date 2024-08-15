93af1cc8bc20782766b40c7a37cac66acaa74126
mutiara-n <mutiara.nafysah11@gmail.com>
2024-08-15 17:39:14 +0700

    feat: add cart functionality

--- frontend/final_project/src/pages/_app.tsx
+++ frontend/final_project/src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import '@glidejs/glide/dist/css/glide.core.min.css';
+import CartProvider from "@/providers/CartProvider";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster toastOptions={{
+        style: {
+          background: "rgb(51 65 85)",
+          color: "#fff"
+        },
+      }}
+      />
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
+    </>
+  )
 }

7c99a562dd985eaf2b877e2adf4e02730d1d4ad9
Ano Jumisa <anojumisa@gmx.de>
2024-08-19 14:26:27 +0700

    feat: add green movement carousel on landing page

--- frontend/final_project/src/pages/index.tsx
+++ frontend/final_project/src/pages/index.tsx
@@ -8,6 +8,7 @@ import CTA_right from "@/components/fragments/CTA_right";
 import CarouselLogo from "@/components/fragments/Carousel_Logo";
 import HeroSection from "@/components/layouts/Landing_Page/HeroSection";
 import ProductCardCategory from "@/components/layouts/Landing_Page/ProductCardCategory";
+import Slider from "@/components/layouts/Landing_Page/GreenProject/Slider";
 import withIntersectionObserver from "@/components/hoc/withIntersectionObserver";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ const CTA_rightWithObserver = withIntersectionObserver(CTA_right);
 const ProjectTeamWithObserver = withIntersectionObserver(ProjectTeam);
 const TestimonialsWithObserver = withIntersectionObserver(Testimonials);
 const FooterWithObserver = withIntersectionObserver(Footer);
+const SliderWithObserver = withIntersectionObserver(Slider);
 
 export default function Home() {
   return (
@@ -28,6 +30,7 @@ export default function Home() {
       <NavbarWithObserver />
       <HeroSectionWithObserver />
       <CarouselLogoWithObserver />
+      <SliderWithObserver />
       <ProductCardCategoryWithObserver />
       <ProductCardCategoryWithObserver />
       <CTA_leftWithObserver />
